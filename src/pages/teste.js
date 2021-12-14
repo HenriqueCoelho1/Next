1c0119ae54dbcfa019cf3e05bffd67eb1c0089b9
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-12-13 22:27:39 -0300

    useState and useEffect using next

--- src/pages/teste.js
+++ src/pages/teste.js
@@ -1,5 +1,21 @@
 import React from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function Teste() {
+    const [users, setUsers] = useState([])
+
+
+    const fetchUsers = async () => {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+        const data = await response.data
+        setUsers(data)
+    }
+
+    console.log(users)
+    useEffect(() => {
+        fetchUsers()
+
+    }, [])
     return <h1>Teste</h1>
 }
