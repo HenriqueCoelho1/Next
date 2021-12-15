3fdc4649f8d6d1c574343dbbcc7419a8d4b77519
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-12-14 22:34:29 -0300

    get server side props

--- src/pages/teste.js
+++ src/pages/teste.js
@@ -2,20 +2,36 @@ import React from "react"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-export default function Teste() {
-    const [users, setUsers] = useState([])
+const Users = ({ users }) => {
+    // const [users, setUsers] = useState([])
 
 
-    const fetchUsers = async () => {
-        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
-        const data = await response.data
-        setUsers(data)
-    }
+    // const fetchUsers = async () => {
+    //     const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+    //     const data = await response.data
+    //     setUsers(data)
+    // }
 
-    console.log(users)
-    useEffect(() => {
-        fetchUsers()
+    // console.log(users)
+    // useEffect(() => {
+    //     fetchUsers()
 
-    }, [])
-    return <h1>Teste</h1>
+    // }, [])
+
+
+    return <div>
+        {users.map((u) => (
+            <div key={u.id}>{u.name}</div>
+        ))}
+    </div>
 }
+
+export async function getServerSideProps(context) {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+    const data = await response.data
+    return {
+        props: { users: data }
+    }
+}
+
+export default Users
